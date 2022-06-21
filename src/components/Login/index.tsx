a7a843f8a9db8846b2ce13924ed4e5e37d556c09
dlwnsgml0328 <junhee@eazel.net>
2022-06-21 11:10:04 +0900

    feat: Login 모델 로직 분류

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,12 +1,42 @@
+import AgoraRTC from 'agora-rtc-sdk-ng';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { config } from '../../config';
 import * as S from './index.styles';
-const Login = () => {
+
+interface IAuth {
+  setAuth: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Login = ({ setAuth }: IAuth) => {
   const [id, setId] = useState('');
+
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (inputRef.current) inputRef.current.focus();
   });
+
+  const client = AgoraRTC.createClient({
+    codec: 'vp8',
+    mode: 'rtc',
+  });
+
+  const { APP_ID, CHANNEL, TOKEN } = config;
+
+  const join = useCallback(
+    async (id: string) => {
+      await client
+        .join(APP_ID, CHANNEL, TOKEN, id)
+        .then((res) => {
+          console.log(`@ join ${res}`);
+        })
+        .catch((err) => {
+          console.error(`@ err occurred in join ${err}`);
+        });
+    },
+
+    [client, APP_ID, CHANNEL, TOKEN]
+  );
 
   const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     if (isNaN(Number(e.target.value))) {
@@ -23,18 +53,31 @@ const Login = () => {
   }, []);
 
   const onSubmit = useCallback(
-    (e: React.FormEvent) => {
+    async (e: React.FormEvent) => {
       e.preventDefault();
-      console.log(`welcome ${id}!`);
 
-      setId('');
+      if (id.length < 3) {
+        alert('Invalid length, must be at least 3 characters');
+        return;
+      }
+
+      await join(id)
+        .then(() => {
+          console.log(`welcome ${id}!`);
+          setId('');
+          setAuth(true);
+        })
+        .catch(() => {
+          console.error(`login failed, redirecting to login page`);
+        });
     },
-    [id]
+    [id, join, setAuth]
   );
 
   return (
     <S.LoginFormWrapper>
       <h3>Login Form</h3>
+
       <form onSubmit={onSubmit}>
         <input
           ref={inputRef}
--- src/pages/CreateRemoteTracks/index.tsx
+++ src/pages/CreateRemoteTracks/index.tsx
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import Login from '../../components/Login';
 
 const CreateRemoteTracks = () => {
+  const [auth, setAuth] = useState(false);
   return (
     <div>
       <h3>Create Remote Tracks</h3>
 
-      <Login />
+      {!auth ? <Login setAuth={setAuth} /> : <div>Welcome!</div>}
     </div>
   );
 };
